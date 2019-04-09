5fd2f91a1005e05b3366ddfcb1f0ebd66ba266a3
Saad Ali <saadali1410@gmail.com>
2019-04-09 13:09:13 +0500

    Add files via upload

---
+++ src/app/documentation/documentation.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+// import './assets/css/owl.carousel.min.css';
+// import './assets/css/owl.theme.default.min.css';
+@Component({
+  selector: 'app-documentation',
+  templateUrl: './documentation.component.html',
+  styleUrls: ['./documentation.component.css']
+})
+export class DocumentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/service/verify.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, Output } from "@angular/core";
+import { EventEmitter } from "events";
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class VerifyService {
+
+  @Output() asisd=new EventEmitter();
+  private domain = 'https://v2.namsor.com/NamSorAPIv2/api2/json/phoneCode';
+httpOptions = {
+    headers: new HttpHeaders({
+      'Accept':  'application/json',
+      'x-api-key': '4ac7667457defcf5e4ebdaa6cb985e5d'
+    })
+  };
+  private urls = {
+      get: `${this.domain}`,
+
+  }
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  get(data) {
+  
+      return this.http.get(`${this.urls.get+'/'+data.fname+'/'+data.lname+'/'+data.number}`, this.httpOptions);
+  }
+}
