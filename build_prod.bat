666cdb9b7e1bd4d922d27ed6562a06d2c45853ae
NamSor <elian.carsenat@namsor.com>
2019-04-20 00:18:58 +0200

    redeploy

--- build_prod.bat
+++ build_prod.bat
@@ -1,3 +1,5 @@
 REM ng build --prod --output-path docs --base-href /phone-validation/
 ng build --prod --output-path docs --base-href /
 copy docs/index.html docs/404.html
+copy src/robots.txt docs/robots.txt
+copy src/sitemap.xml src/sitemap.xml
