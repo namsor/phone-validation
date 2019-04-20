fc5ce0fdb37c914840cf147b8944245df07b9ddf
NamSor <elian.carsenat@namsor.com>
2019-04-20 07:35:01 +0200

    fix

--- build_prod.bat
+++ build_prod.bat
@@ -2,4 +2,4 @@ REM ng build --prod --output-path docs --base-href /phone-validation/
 ng build --prod --output-path docs --base-href /
 copy docs\index.html docs\404.html
 copy src\robots.txt docs\robots.txt
-copy src\sitemap.xml src\sitemap.xml
+copy src\sitemap.xml docs\sitemap.xml
