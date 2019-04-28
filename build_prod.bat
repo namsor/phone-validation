29b3f0c77484f99d12f6baa86cd06116587076ee
NamSor <elian.carsenat@namsor.com>
2019-04-28 13:19:53 +0200

    added google tag manager
    added google tag manager

--- build_prod.bat
+++ build_prod.bat
@@ -3,3 +3,4 @@ ng build --prod --output-path docs --base-href /
 copy docs\index.html docs\404.html
 copy src\robots.txt docs\robots.txt
 copy src\sitemap.xml docs\sitemap.xml
+REM you need to check that the deployment works and usually re-set Github Pages
