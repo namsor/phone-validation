ddfd6d033bead73dcec31b32704843683b49a6ef
NamSor <elian.carsenat@namsor.com>
2019-05-06 15:05:27 +0200

    added scrapping + fix tar vulnerability
    added scrapping + fix tar vulnerability

--- build_prod.bat
+++ build_prod.bat
@@ -4,3 +4,4 @@ copy docs\index.html docs\404.html
 copy src\robots.txt docs\robots.txt
 copy src\sitemap.xml docs\sitemap.xml
 REM you need to check that the deployment works and usually re-set Github Pages
+
